ada6d5b073f6383a658cb61aed408e4cdac1ee75
Carlos <perseto99@gmail.com>
2024-05-09 19:44:30 -0500

    test fix

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,7 @@
 import requests
 
 def test_get_clientes():
-    response = requests.get("http://localhost/clientes/")
+    response = requests.get("http://localhost:8000/clientes/")
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "application/json"
 
